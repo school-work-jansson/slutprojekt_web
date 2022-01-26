8ea8202685a7cbd0f067845844dd835271f3dd12
Linus Jansson <linus.jansson2000@gmail.com>
2022-01-26 20:08:16 +0100

    set view-engine and static folder

--- src/server.js
+++ src/server.js
@@ -1,5 +1,8 @@
 import express from "express";
 import bodyParser from "body-parser";
+import path from "path";
+
+import { __dirname } from "./utils";
 
 // måste användas för att requirea
 // import { createRequire } from 'module';
@@ -10,12 +13,17 @@ const server = express()
 server.use(bodyParser.urlencoded({ extended: false }))
 server.use(bodyParser.json())
 
+
+server.use(express.static(path.join(__dirname, 'static'))); // Static files as stylesheets and scripts
+server.set('views', path.join(__dirname, 'pages')); // SSR webpages (index, contact, profile, login)
+server.set('view engine', 'ejs');
+
 let info = {
   desc: "Det här projektet kommer att vara lite som trustpilot fast istället för att vara inriktad på företag i sig så kommer den att targeta recenssion av matprodukter från olika företag."
 }
 
 server.get('/', (req, res) => { 
-  res.send(`<p>${info.desc}</p>`)
+  res.render('index', {description: info.desc})
 })
 
 server.get('/about', (req, res) => { 
@@ -25,8 +33,8 @@ server.get('/about', (req, res) => {
 import { mailRoute } from './routes/mail'
 server.use('/contact', mailRoute)
 
-import { authRoute } from "./routes/user"
-server.use('/u', authRoute);
+import { userRoute } from "./routes/user"
+server.use('/u', userRoute);
 
 
 const port = 8080
