f788630932f4538d526794fcfaafa449a1ccc9f3
Linus Jansson <linus.jansson2000@gmail.com>
2022-01-27 11:02:44 +0100

    La till session middleware

--- src/server.js
+++ src/server.js
@@ -1,6 +1,9 @@
 import express from "express";
 import bodyParser from "body-parser";
 import path from "path";
+import seession from 'express-session';
+
+import { v4 as uuid } from 'uuid';
 
 import { __dirname } from "./utils";
 
@@ -9,6 +12,30 @@ import { __dirname } from "./utils";
 // const require = createRequire(import.meta.url); 
 
 const server = express()
+
+
+/* 
+  The secret should be a random string of characters. 
+  Ideally you would also change it periodically in case it has been discovered. 
+  However, this requires support for secret rotation so you don't immediately invalidate existing sessions. 
+  That is, two session secrets should be considered valid simultaneously.
+  To my knowledge, Express doesn't have support for rotating secrets at this time. 
+  https://stackoverflow.com/questions/43887865/what-is-a-secure-session-secret
+*/
+let time = 36000
+server.use(session({
+  secret: process.env.SESSION_SECRET,
+  genid: (req) => {
+    return uuid() // use UUIDs for session IDs
+  },
+  // store: 
+  cookie: {
+    maxAge: time
+  }
+}))
+
+import helmet from 'helmet';
+server.use(helmet())
 
 server.use(bodyParser.urlencoded({ extended: false }))
 server.use(bodyParser.json())
@@ -41,7 +68,7 @@ server.all('*', (req, res) => {
   res.render('404');
 });
 
-const port = 8080
-server.listen(port, () => {
-  console.log(`Server is up and running http://localhost:${port}`)
+let PORT = process.env.PORT || 8080
+server.listen(PORT, () => {
+  console.log(`Server is up and running http://localhost:${PORT}`)
 })
