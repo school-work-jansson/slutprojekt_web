eba499c387e00674453e9706e6a80171bf6ab781
Linus Jansson <linus.jansson2000@gmail.com>
2022-02-15 17:53:41 +0100

    Reviderat databasen litegrann

--- prototyp/databas/slutproject_databas_schema.sql
+++ prototyp/databas/slutproject_databas_schema.sql
@@ -13,7 +13,7 @@ CREATE TABLE `users` (
 
 CREATE TABLE `reports` (
   `id` int PRIMARY KEY AUTO_INCREMENT,
-  `reporter_id` varchar(255) NOT NULL,
+  `user_id` int NOT NULL,
   `review_id` int NOT NULL,
   `resolved` boolean DEFAULT false,
   `optional` varchar(255)
@@ -21,8 +21,6 @@ CREATE TABLE `reports` (
 
 CREATE TABLE `reviews` (
   `id` int PRIMARY KEY AUTO_INCREMENT,
-  `poster_id` int NOT NULL,
-  `product_id` int NOT NULL,
   `rating` int NOT NULL,
   `title` varchar(255) NOT NULL,
   `content` varchar(255),
@@ -33,18 +31,31 @@ CREATE TABLE `reviews` (
 
 CREATE TABLE `products` (
   `id` int PRIMARY KEY AUTO_INCREMENT,
-  `poster_id` int NOT NULL,
+  `user_id` int NOT NULL,
   `product_picture` varchar(255),
   `name` varchar(255) NOT NULL,
-  `description` varchar(255) NOT NULL,
-  `category_id` int
+  `description` varchar(255) NOT NULL
 );
+
+CREATE TABLE `user_reviews` (
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `user_id` int NOT NULL REFERENCES users(id) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  `review_id` int NOT NULL REFERENCES reviews(id) ON DELETE NO ACTION ON UPDATE NO ACTION
+);
+
+CREATE TABLE `product_reviews` (
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `review_id` int NOT NULL REFERENCES reviews(id) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  `product_id`int NOT NULL REFERENCES products(id) ON DELETE NO ACTION ON UPDATE NO ACTION
+);
+
+
 
 -- Refererar till att att en product har en poster id från users
 ALTER TABLE `products` 
   ADD 
-    CONSTRAINT fk_products_users 
-    FOREIGN KEY (poster_id) 
+    CONSTRAINT fk_products_poster_id
+    FOREIGN KEY (user_id) 
     REFERENCES users(id)
   ON DELETE NO ACTION 
   ON UPDATE NO ACTION;
@@ -53,12 +64,10 @@ ALTER TABLE `products`
 ALTER TABLE `reports` 
   ADD 
     CONSTRAINT fk_reports_reporter
-    FOREIGN KEY (reporter_id) 
+    FOREIGN KEY (user_id) 
     REFERENCES users(id) 
   ON DELETE NO ACTION 
   ON UPDATE NO ACTION;
--- ERROR 3780 (HY000): Referencing column 'reporter_id' and referenced column 'id' in foreign key constraint 'fk_reports_reporter' are incompatible.
-
 
 
 -- reports har en review som är raporterad
@@ -70,36 +79,17 @@ ALTER TABLE `reports`
   ON DELETE NO ACTION 
   ON UPDATE NO ACTION;
 
--- 
-ALTER TABLE `reviews` 
-  ADD 
-    CONSTRAINT fk_review_product
-    FOREIGN KEY (product_id) 
-    REFERENCES products(id) 
-  ON DELETE NO ACTION 
-  ON UPDATE NO ACTION;
-
--- users kan ha en review och refereras till poster id i reviews
-ALTER TABLE `users` 
-  ADD 
-    CONSTRAINT fk_user_review 
-    FOREIGN KEY (id) 
-    REFERENCES reviews(poster_id) 
-  ON DELETE NO ACTION 
-  ON UPDATE NO ACTION;
--- https://stackoverflow.com/questions/43511183/mysql-error-1822-failed-to-add-foreign-key-constraint-missing-index-for-contra
--- ERROR 1822 (HY000): Failed to add the foreign key constraint. Missing index for constraint 'fk_user_review' in the referenced table 'reviews'
 
 
 -- Testing values
 INSERT 
-  INTO `products` (`poster_id`, `name`, `description`) 
+  INTO `products` (`user_id`, `name`, `description`) 
   VALUES (1, 'Test product name', 'lorem ipsum');
 
 INSERT 
-  INTO `reviews` (`poster_id`, `product_id`, `rating`, `title`, `content`, `created_at`) 
+  INTO `reviews` (`user_id`, `product_id`, `rating`, `title`, `content`, `created_at`) 
   VALUES (1, 1, 5, 'Test product review title', 'lorem ipsum', NOW());
 
 INSERT 
-  INTO `reviews` (`poster_id`, `product_id`, `rating`, `title`, `content`, `created_at`) 
+  INTO `reviews` (`user_id`, `product_id`, `rating`, `title`, `content`, `created_at`) 
   VALUES (1, 1, 5, 'Test product review title2', 'lorem ipsum2', NOW());
