a7c90674049d5646b1e2c012e8547c40d6e8d989
Linus Jansson <linus.jansson2000@gmail.com>
2022-02-19 18:39:35 +0100

    Möjlighet att uppdatera sin profile på profile.ejs

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,11 +1,41 @@
 import express from "express";
 const router = express.Router()
 
+import { User } from "../database";
+import { session_check } from "../middleware";
+
 // https://www.w3schools.com/nodejs/nodejs_email.asp
 
 router.get('/hello_world', (req, res) => { 
     res.send({status: 200});
 })
   
+router.post('/update_profile', session_check, (req, res) => {
+    let user_data = req.body;
+
+    // om användaren inte skickar med något användarnamn
+    req.session.client_data.username = user_data.username;
+    req.session.client_data.email = user_data.email;
+
+    console.log(user_data);
+    
+    (async () => {
+        let user = new User(req.session.client_data.id);
+        // Eftersom att jag måste lagra refresh_token så "initlizar" jag användaren
+        // sedan uppdaterar jag databasen med ny data ifall användaren vill byta namn eller epost
+        let result = await user.update(req.session.client_data);
+        
+        console.log(result);
+
+    })();
+
+    res.redirect("/u/profile");
+    
+})
+
+router.delete('/remove_user', session_check, (req, res) => {
+    console.log("User requested account deletion")
+    res.redirect('index')
+})
 
 export {router as apiRoute}
