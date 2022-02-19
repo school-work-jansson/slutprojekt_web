0fa84324938a8a8f5b975c6a2048da27f0717f52
Linus Jansson <linus.jansson2000@gmail.com>
2022-02-19 18:39:55 +0100

    Fixade query för genererad data

--- prototyp/databas/database_queries.sql
+++ prototyp/databas/database_queries.sql
@@ -1,19 +1,18 @@
 -- Testing values
-INSERT 
-  INTO `products` (`user_id`, `name`, `description`) 
-  VALUES (1, 'tmp product', 'lorem ipsum product');
+START TRANSACTION;
+  INSERT 
+    INTO `products` (`name`, `description`) 
+    VALUES ('tmp product', 'lorem ipsum product');
+  SET @PRODUCT_ID =(SELECT LAST_INSERT_ID() );    
+  INSERT 
+    INTO `reviews` (`rating`, `title`, `content`, `created_at`) 
+    VALUES (1, 'Sämst', 'lorem ipsum', NOW());
+  SET @REVIEW_ID = (SELECT LAST_INSERT_ID() );    
+  INSERT 
+    INTO `product_reviews` (`user_id`, `review_id`, `product_id`) 
+    VALUES ((SELECT id FROM users WHERE discord_id = '322015089529978880'), @REVIEW_ID, @PRODUCT_ID);
+COMMIT;
 
-INSERT 
-  INTO `reviews` (`rating`, `title`, `content`, `created_at`) 
-  VALUES (5, 'Test review review title', 'lorem ipsum', NOW());
-
-INSERT 
-  INTO `product_reviews` (`review_id`, `product_id`) 
-  VALUES (1, 1);
-
-INSERT 
-  INTO `user_reviews` (`user_id`, `review_id`) 
-  VALUES (1, 1);
 
 
 -- POST product_review
@@ -27,6 +26,9 @@ START TRANSACTION;
 COMMIT;
 
 -- Hämta user reviews
+SET @anyVariableName=(SELECT yourColumnName FROM yourTableName WHERE yourCondition);
+SET @anyVariableName=(SELECT yourColumnName FROM yourTableName WHERE yourCondition);
+SET @anyVariableName=(SELECT yourColumnName FROM yourTableName WHERE yourCondition);
 SELECT 
     u.profile_picture, u.nickname, 
     r.rating, r.title, r.content, r.created_at, 
@@ -37,6 +39,8 @@ FROM product_reviews pr
 	INNER JOIN products p ON ( pr.product_id = p.id  )  
 WHERE (SELECT id FROM users WHERE discord_id = ?)
 
+
+SELECT u.profile_picture, u.nickname, r.rating, r.title, r.content, r.created_at, p.name FROM product_reviews pr INNER JOIN users u ON ( pr.user_id = u.id  )  INNER JOIN reviews r ON ( pr.review_id = r.id  )  INNER JOIN products p ON ( pr.product_id = p.id  )  WHERE (SELECT id FROM users WHERE discord_id = ?)
 
 -- GET product & reviews
 SELECT 
