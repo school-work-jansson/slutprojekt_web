abf52c13935b8f2a4592ffcf0b830891feaf8057
Linus Jansson <linus.jansson2000@gmail.com>
2022-03-21 12:20:44 +0100

    ...

--- src/routes/product.js
+++ src/routes/product.js
@@ -1,19 +1,8 @@
 import express from "express";
-const router = express.Router()
 import { Product } from '../database';
-import { session_check } from "../middleware";
+import { session_check, save_last_site_visited } from "../middleware";
 
-/* PÅ något sätt ska den ha ett unikt "id" för att kunna separera två olika typer av samma produkt */
-// router.get("/:name", (req, res) => {
-//     // Check database for name
-//     fetched_product, fetched_reviews, error = get_product(req.params.name)
-//     // If result from database check return product
-    
-//         // Get product info if name exists
-//         // return to user
-//         res.render('product', {product: fetched_product, reviews: fetched_reviews, error: null})
-// })
-
+const router = express.Router()
 
 router.get('/:hash', async (req, res, next) => {
     
@@ -22,12 +11,14 @@ router.get('/:hash', async (req, res, next) => {
     let product = new Product();
     let [fetched_product, fetched_reviews, error] = await product.fetch(req.params.hash);
     
-    if (error != null) return res.send(error);
+    if (error) return next(error);
+
     // console.log({product: fetched_product,  reviews: fetched_reviews});
-    res.render('product', {product: fetched_product,  reviews: fetched_reviews})
+    res.render('product', {user: req.session.client_data, product: fetched_product,  reviews: fetched_reviews})
     // res.render('product', {product: fetched_product, reviews: fetched_reviews})
 })
 
+// Ifall man skulle vilja se rådatan så kan man lägga till /raw för att endast se objektet och inte någon html
 router.get('/:hash/raw', async (req, res) => {
     
     if (!req.params.hash) return next("No product");
@@ -35,23 +26,12 @@ router.get('/:hash/raw', async (req, res) => {
     let product = new Product();
     let [fetched_product, fetched_reviews, error] = await product.fetch(req.params.hash);
     
-    if (error != null) return res.send(error);
+    if (error != null) return next(error);
     
     // console.log({product: fetched_product,  reviews: fetched_reviews});
-    res.send({product: fetched_product,  reviews: fetched_reviews})
+    res.send({user: req.session.client_data, product: fetched_product,  reviews: fetched_reviews})
     // res.render('product', {product: fetched_product, reviews: fetched_reviews})
 })
-
-
-router.get('/p_product', session_check, (req, res) => {
-    res.render('product')
-})
-
-router.get('/p_review', (req, res) => {
-    res.render('review')
-})
-
-
 
 router.post('/post_product', session_check, async (req, res) => {
     let product = new Product();
@@ -59,9 +39,6 @@ router.post('/post_product', session_check, async (req, res) => {
     await product.post_product(req.body, req.session.client_data.id);
     res.redirect('/p/p_product')
 })
-
-
-// router.get("/get_products", (req, res))
 
 export { router as productRoute };
 
