03e3822e3a826b7cc094433e5a952fd7d89a816c
Linus Jansson <linus.jansson2000@gmail.com>
2022-03-23 23:14:19 +0100

    Ändrade om review formen lite

--- src/static/scripts/product.js
+++ src/static/scripts/product.js
@@ -1,45 +1,98 @@
+// TODO: Kanske skriva om den här koden va?
+
+function contentEditable() {
+    const ele = document.getElementById('__review-title');
+
+
+
+    // Get the placeholder attribute
+    const placeholder = ele.getAttribute('data-placeholder');
+    $("#__review-title").keypress(() => {
+        if($("#__review-title").text.length >= 10) Event.preventDefault();
+    })
+    
+
+
+
+    // Set the placeholder as initial content if it's empty
+    ele.innerHTML === '' && (ele.innerHTML = placeholder);
+    ele.addEventListener('focus', function (e) {
+        const value = e.target.innerHTML;
+        value === placeholder && (e.target.innerHTML = '');
+    });
+
+    ele.addEventListener('blur', function (e) {
+        const value = e.target.innerHTML;
+        value === '' && (e.target.innerHTML = placeholder);
+    });
+}
+
+function validKeycodes(code) {
+    switch (code) {
+        case 65:
+        case 67:
+        case 86:
+        case 88:
+        case 8:
+        case 37:
+        case 38:
+        case 39:
+        case 40:
+        case 16:
+        case 36:
+        case 35:
+        case 33:
+        case 34:
+            return true;
+        default:
+            return false
+            break;
+    }
+    
+}
+
+
 function handle_review_form() {
+    // contentEditable()
 
     /* Star-rating Inspiration (https://codepen.io/Leoschmittk/pen/OJbQBgb) */
+    let stars = $('.rating__star.rating-changable').map((_, element) => {return element}).get()    // Mappar alla raiting__star iconerna till en array
     const star_whole = "rating__star rating-changable fas fa-star";
-    const star_half = "raiting__star rating-changable fas fa-star-half-alt"
     const star_none = "rating__star rating-changable far fa-star";
+    let submit_button = $("#submit-review-button");
+    let title_element = $('#title, #__review-title')
+    let content_element = $("#__review-content");
 
-    // Mappar alla raiting__star iconerna till en array
-    let stars = $('.rating__star.rating-changable').map((_, element) => {return element}).get()
+    // Listner som preventar att titeln blir längre än 55 chars
+    $(title_element).on('keydown paste', (event) => { //Prevent on paste as well
+        if ( $(title_element).text().length >= 55 && event.keyCode != 8) event.preventDefault();
+        // console.log($("#__review-title").text(), $("#__review-title").text().length, $("#__review-title").text().length >= 55 && event.keyCode != 8)
+        // TODO: Key code för backspace pilarna, ctrl + a, c, v, r, x
+        // Man ska inte kunna cp pasta in text som är längre än gränsen
+        
+        // let sliced = $("#__review-title").text().slice(0, 55)
+        // $("#__review-title").text(sliced)
+    });
 
-    /* När användaren faktiskt skickar reviewn mot servern */
-    $("#send_review_button").click(() => {
+    /* Listner som väntar på ändringar i review formuläret*/
+    // Test on change
+    title_element.on('input', () => {
+        review_object.title = title_element.text();
+    });
+
+    content_element.on('input', () => {
+        review_object.content = content_element.text()
+    });
+
+    /* Användaren klickar på submit */
+    submit_button.click(() => {
+        console.log("submiting", review_object);
+        submit_button.addClass("active");
         post_review(review_object)
     })
 
-    /* Listner som väntar på ändringar i review formuläret*/
-    $('#title').on('input', () => {
-        let title_value = $("#title").val()
-        $("#preview-title").html(title_value)
-        $("#preview-date").html(getDate())
-        review_object.title = title_value
-    });
-
-    $('#content').on('input', () => {
-        let content_value = $("#content").val()
-        $("#preview-content").html(content_value)
-        $("#preview-date").html(getDate())
-
-        review_object.content = content_value
-    });
-
-    $('#name').on('input', () => {
-        let name_value = $("#name").val()
-        $("#preview-name").html(name_value)
-        $("#preview-date").html(getDate())
-        review_object.name = name_value
-    });
-
-    // console.log("DEBUG: ", stars)
-
     // på något sätt fungerar detta för att ändra mängden stjärnor
-    // När en användare clickar på 
+    // När en användare clickar på en stjärna
     $(stars).each((star_index) => {
         $(stars[star_index]).click(() => {
             let i = star_index;
@@ -57,45 +110,17 @@ function handle_review_form() {
                 
             }
 
-            update_preview_stars(review_object.rating)
-            
-            // console.log("Current rating", review_object.rating)
-
         })
     })
 
 }
 
-function update_preview_stars(rating)
-{
-
-    const preview_star_whole = "preview__rating__star fas fa-star";
-    const preview_star_none = "preview__rating__star far fa-star";
-
-    let preview_stars = $('.preview__rating__star').map((_, element) => {return element}).get()
-
-    $(preview_stars).each((star_index) => {
-        let i = rating - 1
-        
-        if ( $(preview_stars[i]).hasClass(preview_star_whole) )
-        {
-            for (i; i < preview_stars.length; i++)
-                $(preview_stars[i]).attr("class", preview_star_none)
-        }
-        else {
-            for (i; i >= 0; --i)
-                $(preview_stars[i]).attr("class", preview_star_whole)
-        }
-        
-    })   
-
-}
 
 function post_review(review) {
     // $.ajax()
     let validated = validateReviewForm()
     
-    if (!validated[0]) return $("#submit_result").html(`<h3>${validated[1]}<h3>`);
+    if (!validated[0]) return $("#submit_result span").text(`${validated[1]}`);
 
     $.ajax({
         url: `/api/post_review`,
@@ -105,18 +130,16 @@ function post_review(review) {
         async: false,
         success: (res) => {
             console.log("post result", res)
-            if (res)
-            {
+            if (res) {
 
-                $("#submit_result").html(`<h3>Din recenssion är inskickad!<h3>`)
+                $("#submit_result span").text(`Din recenssion är inskickad!`)
                 $("#review-form input, #review-form textarea").prop('disabled', true);
 
-                // setTimeout(() => {
-
-                // }, 5000)
             }
-            else
+            else {
                 $("#submit_result").html(`<h3>Tyvärr var det något som inte riktigt fungerade där. Pröva gärna lite senare!<h3>`)
+            }
+                
         }
     });
 
